87f0eb4945c10b42e08a40f41e7ab609518ce6da
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2021-05-31 16:36:29 +0200

    modification du fichier modalForm

--- src/apps/LC-27/components/modals/modalForm.vue
+++ src/apps/LC-27/components/modals/modalForm.vue
@@ -1,35 +1,23 @@
 <template>
     <b-modal id="modal-scoped">
-    <template #modal-header="{ close }">
-      <!-- Emulate built in modal header close button action -->
-      <b-button size="sm" variant="outline-danger" @click="close()">
-        Close Modal
-      </b-button>
-      <h5>Modal Header</h5>
+   
+
+    <template>
+      <p>Je suis un modal</p>
     </template>
 
-    <template #default="{ hide }">
-      <p>Modal Body with button</p>
-      <b-button @click="hide()">Hide Modal</b-button>
-    </template>
-
-    <template #modal-footer="{ ok, cancel, hide }">
-      <b>Custom Footer</b>
-      <!-- Emulate built in modal footer ok and cancel button actions -->
-      <b-button size="sm" variant="success" @click="ok()">
-        OK
-      </b-button>
+    <template #modal-footer="{ ok, cancel }">
       <b-button size="sm" variant="danger" @click="cancel()">
-        Cancel
+        Annuler
       </b-button>
-      <!-- Button with custom close trigger value -->
-      <b-button size="sm" variant="outline-secondary" @click="hide('forget')">
-        Forget it
+      <b-button size="sm" variant="success" @click="ok()">
+        Valider
       </b-button>
     </template>
   </b-modal>
 </template>
 <script>
 export default {
+    
 }
 </script>
