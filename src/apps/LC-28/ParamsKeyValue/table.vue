054e60cb8b1679995b55273b3b1141714ddaae81
robinrenau <robinrenau@gmail.com>
2021-06-01 13:30:28 +0200

    changement couleur categorie + ajout initial categorie

--- src/apps/LC-28/ParamsKeyValue/table.vue
+++ src/apps/LC-28/ParamsKeyValue/table.vue
@@ -2,7 +2,7 @@
 <div>
 <div class="row">
             <div class="col-md-3 col-xl-2">
-                <div class="card mb-3 widget-content bg-night-fade">
+                <div class="card mb-3 widget-content bg-alternate">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
                             <div class="widget-heading">Modes de règlement</div>
@@ -15,7 +15,7 @@
                 </div>
             </div>
             <div class="col-md-3 col-xl-2">
-                <div class="card mb-3 widget-content bg-arielle-smile">
+                <div class="card mb-3 widget-content bg-warning">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
                             <div class="widget-heading">Membres</div>
@@ -28,7 +28,7 @@
                 </div>
             </div>
             <div class="col-md-3 col-xl-2">
-                <div class="card mb-3 widget-content bg-happy-green">
+                <div class="card mb-3 widget-content bg-success">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
                             <div class="widget-heading">Édition de contenu</div>
@@ -41,7 +41,7 @@
                 </div>
             </div>
             <div class="col-md-3 col-xl-2">
-                <div class="card mb-3 widget-content bg-love-kiss">
+                <div class="card mb-3 widget-content bg-danger">
                     <div class="widget-content-wrapper text-white">
                         <div class="widget-content-left">
                             <div class="widget-heading">Salles de réunion</div>
@@ -68,34 +68,9 @@
             </div>
 </div>
 <div class="main-card mb-3 card">
-            <div class="card-header" style="display:flex; justify-content:space-between">
-                <div >
-                    <b-form-group horizontal  class="mb-0">
-            <b-input-group>
-              <b-form-select v-model="sortDirection">
-                <option value="asc">Modes de règlement</option>
-                <option value="asc">Membres</option>
-                <option value="desc">Édition de contenu</option>
-                <option value="last">Salles de réunion</option>
-                <option value="last">Réglages</option>
-              </b-form-select>
-            </b-input-group>
-          </b-form-group>
-                </div>
-                
-                <div>
-                    <b-form-group horizontal class="mb-0">
-                        <b-input-group>
-                        <b-form-input v-model="filter" placeholder="Recherche" />
-                        <b-input-group-append>
-                            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
-                        </b-input-group-append>
-                        </b-input-group>
-                     </b-form-group> 
-                </div>
-            </div>
+            
     <div class="table-responsive">
-                <table class="align-middle text-truncate mb-0 table table-borderless table-hover">
+                <table class="align-middle mb-0 table table-borderless table-striped table-hover">
                     <thead>
                     <tr>
                         
@@ -106,15 +81,15 @@
                         <th class="text-center">Actions</th>
                     </tr>
                     </thead>
-                    
+                  
                     <tbody v-for="item in items" :key="item.id">
                   
                     <tr >
                         
-                        <div style="display: flex;justify-content: center; align-items: center; margin-top : 10px; margin-left:30px" class="swatch-holder" :class=item.classcategory>
+                        <div style="display: flex;justify-content: center; align-items: center; margin-top : 10px; margin-left:30px" class="swatch-holder swatch-holder-md" :class=item.classcategory>
                          
-                            <td> 
-
+                            <td class="text-center" style="font-weight :bold; color:white; opacity: .9; font-size : 11px"> 
+                                {{item.initial}}
                             </td>
                         </div>
                         <td class="text-center">{{item.key}}</td>
@@ -135,9 +110,7 @@
                             </div>
                             <div role="group" class="btn-group-sm btn-group" >
                                 <modal></modal>
-                                 
-                                 
-                                
+               
                             </div>
                             
                         </td>
@@ -163,14 +136,14 @@ import modal from "./modal.vue"
         },
         data: () => ({
                 items: [
-        { category: 'édition de contenu', key: 'COWORKING_BOOK_INFO', value: "La colloc met à disposition de ses membres qui sont dans l'incapacité de faire du ...", info:'lorem ipsum', classcategory:'bg-happy-green' },
-        { category: 'modes de règlement', key: 'PENDING', value: '01009017', info:'lorem ipsum', classcategory:'bg-night-fade' },
-        { category: 'salles de réunion', key: 'REFUND_DEPOSIT_PERCENT', value: '30', info:'lorem ipsum', classcategory:'bg-love-kiss' },
-        { category: 'réglages', key: 'COWORKING_NUMBER', value: '10', info:'lorem ipsum', classcategory:'bg-night-sky' },
-        { category: 'Membres', key: 'WELCOME_CREDITS', value: '10', info:'Nombre de co offerts lors du premier abonnement à la Colloc', classcategory:'bg-arielle-smile' },
-        { category: 'modes de règlement', key: 'SEPA', value: '01009011', info:'lorem ipsum', classcategory:'bg-night-fade' },
-        { category: 'salles de réunion', key: 'MEETINGROOM_BOOKING_HOUR_START', value: '08:00:00', info:'Heure minimum de début de location des salles de réunion', classcategory:'bg-love-kiss' },
-        { category: 'Membres', key: 'FREETIME', value: '4', info:"Nombre d'heures de location de salle", classcategory:'bg-arielle-smile' },
+        { category: 'édition de contenu', key: 'COWORKING_BOOK_INFO', value: "La colloc met à disposition de ses membres qui sont dans l'incapacité de faire du ...", info:'lorem ipsum', classcategory:'bg-success', initial:'ÉdC' },
+        { category: 'modes de règlement', key: 'PENDING', value: '01009017', info:'lorem ipsum', classcategory:'bg-alternate', initial:'MdR' },
+        { category: 'salles de réunion', key: 'REFUND_DEPOSIT_PERCENT', value: '30', info:'lorem ipsum', classcategory:'bg-danger', initial:'SdR' },
+        { category: 'réglages', key: 'COWORKING_NUMBER', value: '10', info:'lorem ipsum', classcategory:'bg-night-sky', initial:'R' },
+        { category: 'Membres', key: 'WELCOME_CREDITS', value: '10', info:'Nombre de co offerts lors du premier abonnement à la Colloc', classcategory:'bg-warning', initial:'M' },
+        { category: 'modes de règlement', key: 'SEPA', value: '01009011', info:'lorem ipsum', classcategory:'bg-alternate', initial:'MdR' },
+        { category: 'salles de réunion', key: 'MEETINGROOM_BOOKING_HOUR_START', value: '08:00:00', info:'Heure minimum de début de location des salles de réunion', classcategory:'bg-danger', initial:'SdR' },
+        { category: 'Membres', key: 'FREETIME', value: '4', info:"Nombre d'heures de location de salle", classcategory:'bg-warning', initial:'M' },
         
 
         
