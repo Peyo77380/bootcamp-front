de7c2d91b980e64e382e71735259e23e6f3a4e5d
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2021-06-21 11:13:42 +0200

    Première réflexion esthétique sur le composant

--- src/apps/LC-163/components/customHeader.vue
+++ src/apps/LC-163/components/customHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="headerContainer">
-        <img src="" alt="Logo de l'entreprise">
+        <img src="../../../assets/images/Logo/Logo bleue marine.png" width="50px" height="50px" alt="Logo de l'entreprise">
+        <Breadcrumb />
         <h1>test de titre</h1>
         <p>Test de description</p>
-        <Breadcrumb />
     </div>
 </template>
 <script>
