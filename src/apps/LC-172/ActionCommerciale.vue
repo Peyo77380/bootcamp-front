4bb1b160d970c0928425004855af54bf4800af01
Ange-BC <andriniaina.randria@gmail.com>
2021-06-22 22:26:26 +0200

    crud todolist

--- src/apps/LC-172/ActionCommerciale.vue
+++ src/apps/LC-172/ActionCommerciale.vue
@@ -1,150 +1,58 @@
 <template>
 <div>
     <h3 class="drawer-heading">Mes actions commerciales</h3>
-    <div class="todo-box col-12">
+    <div class="card-header-tab card-header">
+        <div class="btn-actions-pane-right text-capitalize actions-icon-btn">
+            <button type="button" class="btn mr-2 mb-2 p-2 mt-2 btn-pill btn-icon btn-success ">
+                <i class="btn-icon-wrapper lnr-plus-circle"></i> Ajouter une nouvelle Action
+            </button>
+        </div>
+    </div>
+    <div class="todo-box">
         <ul class="todo-list-wrapper list-group list-group-flush">
-            <li class="list-group-item">
+            <li class="list-group-item"
+            v-for="action in commercialData" :key="action.id"
+            >
                 <v-expansion-panel>
-                    <v-expansion-panel-content>
+                    <v-expansion-panel-content hide-actions>
                             <template v-slot:header>
                                 <div class="todo-indicator bg-warning"></div>
                                 <div class="widget-content p-0">
                                     <div class="widget-content-wrapper">
                                         <div class="widget-content-left">
-                                            <div class="widget-heading"> Architecture07-Architecture dplg
+                                            <div class="widget-heading"> {{ action.name }}
                                                 <div class="badge badge-danger ml-2">En cours</div>
                                             </div>
-                                            <div class="widget-subheading"><i>Rappel téléphonique</i></div>
+                                            <div class="widget-subheading"><i>{{ action.action }}</i></div>
                                         </div>
                                         <div class="widget-content-right">
                                             <button type="button" class="btn mb-2 mr-2 btn-icon btn-icon-only btn-pill btn-primary">
                                                 <i class="lnr-pencil btn-icon-wrapper"></i>  
                                             </button>
-                                            <button type="button" class=" btn mb-2 mr-2 btn-icon btn-icon-only btn-pill btn-warning">
+                                            <!-- <button type="button" class=" btn mb-2 mr-2 btn-icon btn-icon-only btn-pill btn-warning">
                                                 <i class="lnr-trash btn-icon-wrapper"></i>  
-                                            </button>
+                                            </button> -->
                                         </div>
                                     </div>
                                 </div>
                             </template>  
                             <v-card>  
                                 <v-card-text>
-                                    <div class="widget-subheading"><i>Rappel téléphonique</i></div>
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left">
+                                                <div class="widget-subheading"><i>{{ action.detail }}</i></div><br>
+                                                <div class="widget-heading">{{ action.responsable }}</div>
+                                            </div>
+                                            <div class="widget-content-right">
+                                                <div class="widget-subheading"><i>Échéance: {{ action.date }}</i></div>
+                                            </div>    
+                                        </div>
+                                    </div>
                                 </v-card-text>          
                             </v-card>  
                     </v-expansion-panel-content>
                 </v-expansion-panel>
-            </li>
-            <li class="list-group-item">
-                <div class="todo-indicator bg-focus"></div>
-                <div class="widget-content p-0">
-                    <div class="widget-content-wrapper">
-                        <div class="widget-content-left mr-2">
-                            <div class="custom-checkbox custom-control">
-                                <input type="checkbox" id="exampleCustomCheckbox1666" class="custom-control-input">
-                                <label class="custom-control-label" for="exampleCustomCheckbox1666">&nbsp;</label></div>
-                        </div>
-                        <div class="widget-content-left">
-                            <div class="widget-heading">Task with hover dropdown menu</div>
-                            <div class="widget-subheading">
-                                <div>By Johnny
-                                    <div class="badge badge-pill badge-info ml-2">NEW</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="widget-content-right widget-content-actions">
-                            <div class="d-inline-block">
-                                <b-dropdown toggle-class="btn-icon btn-icon-only border-0 btn-transition" variant="link" no-caret>
-                                    <span slot="button-content"><font-awesome-icon icon="ellipsis-h" /></span>
-                                    <div>
-                                        <button type="button" disabled="" tabindex="-1" class="disabled dropdown-item">Action</button>
-                                        <button type="button" tabindex="0" class="dropdown-item">Another Action</button>
-                                        <div tabindex="-1" class="dropdown-divider"></div>
-                                        <button type="button" tabindex="0" class="dropdown-item">Another Action</button>
-                                    </div>
-                                </b-dropdown>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="list-group-item">
-                <div class="todo-indicator bg-primary"></div>
-                <div class="widget-content p-0">
-                    <div class="widget-content-wrapper">
-                        <div class="widget-content-left mr-2">
-                            <div class="custom-checkbox custom-control">
-                                <input type="checkbox" id="exampleCustomCheckbox4777" class="custom-control-input">
-                                <label class="custom-control-label" for="exampleCustomCheckbox4777">&nbsp;</label></div>
-                        </div>
-                        <div class="widget-content-left flex2">
-                            <div class="widget-heading">Badge on the right task</div>
-                            <div class="widget-subheading">This task has show on hover actions!</div>
-                        </div>
-                        <div class="widget-content-right widget-content-actions">
-                            <button class="border-0 btn-transition btn btn-outline-success">
-                                <font-awesome-icon icon="check" />
-                            </button>
-                        </div>
-                        <div class="widget-content-right ml-3">
-                            <div class="badge badge-pill badge-success">Latest Task</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="list-group-item">
-                <div class="todo-indicator bg-info"></div>
-                <div class="widget-content p-0">
-                    <div class="widget-content-wrapper">
-                        <div class="widget-content-left mr-2">
-                            <div class="custom-checkbox custom-control">
-                                <input type="checkbox" id="exampleCustomCheckbox2444" class="custom-control-input">
-                                <label class="custom-control-label" for="exampleCustomCheckbox2444">&nbsp;</label></div>
-                        </div>
-                        <div class="widget-content-left mr-3">
-                            <div class="widget-content-left"><img width="42" class="rounded" src="@/assets/images/avatars/1.jpg" alt=""/></div>
-                        </div>
-                        <div class="widget-content-left">
-                            <div class="widget-heading">Go grocery shopping</div>
-                            <div class="widget-subheading">A short description ...</div>
-                        </div>
-                        <div class="widget-content-right widget-content-actions">
-                            <button class="border-0 btn-transition btn btn-sm btn-outline-success">
-                                <font-awesome-icon icon="check" />
-                            </button>
-                            <button class="border-0 btn-transition btn btn-sm btn-outline-danger">
-                                <font-awesome-icon icon="trash-alt" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="list-group-item">
-                <div class="todo-indicator bg-success"></div>
-                <div class="widget-content p-0">
-                    <div class="widget-content-wrapper">
-                        <div class="widget-content-left mr-2">
-                            <div class="custom-checkbox custom-control">
-                                <input type="checkbox" id="exampleCustomCheckbox3222" class="custom-control-input">
-                                <label class="custom-control-label" for="exampleCustomCheckbox3222">&nbsp;</label></div>
-                        </div>
-                        <div class="widget-content-left flex2">
-                            <div class="widget-heading">Development Task</div>
-                            <div class="widget-subheading">Finish React ToDo List App</div>
-                        </div>
-                        <div class="widget-content-right">
-                            <div class="badge badge-warning mr-2">69</div>
-                        </div>
-                        <div class="widget-content-right">
-                            <button class="border-0 btn-transition btn btn-outline-success">
-                                <font-awesome-icon icon="check" />
-                            </button>
-                            <button class="border-0 btn-transition btn btn-outline-danger">
-                                <font-awesome-icon icon="trash-alt" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
             </li>
         </ul>
     </div>
@@ -154,7 +62,12 @@
 </template>
     
 <script>
+import { commercialData } from "@/apps/LC-172/commercialData";
 export default {
-    
+    data() {
+        return {
+            commercialData: commercialData,
+        }  
+    }
 }
 </script>
