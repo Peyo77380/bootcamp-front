40a4d796297c329133ff7df08d030deacf2c1741
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2021-06-23 17:59:00 +0200

    modif url index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/",
-    timeout: 300,
+    baseURL: "http://localhost/api/",
+    timeout: 3000,
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
