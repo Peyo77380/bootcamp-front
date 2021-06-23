ad68b3cb9a7e38b48526371b8d28307697cb092c
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2021-06-23 21:33:11 +0200

    création de global.js

---
+++ src/mixins/global.js
@@ -0,0 +1,27 @@
+import api from '@/services/axios';
+
+export const Globals = {
+
+    methods: {
+
+        async getGlobals() {
+
+            try {
+                const response = await api.get('list')
+                console.log('from mixins', response);
+                return {
+                    status: 1,
+                    datas: response.data.datas
+                }
+            } catch (error) {
+                return {
+                    status: 0,
+                    error
+                }
+            }
+
+        }
+
+    }
+
+}
--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -3,6 +3,7 @@ import axios from "axios"
 const api = axios.create({
     baseURL: "http://localhost/api/",
     timeout: 3000,
+    withCreditentials: false,
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
