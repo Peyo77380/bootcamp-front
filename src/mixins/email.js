fee12b09ceeeceb273ca14503b4d70fcc0d4c72a
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-06-25 14:49:26 +0200

    update comment

--- src/mixins/email.js
+++ src/mixins/email.js
@@ -4,7 +4,7 @@ export const Emails = {
     methods: {
         async getAllEmails() {
             try {
-                // send wl, section, lang dynamically
+                // send section, wl, lang dynamically
                 const response = await api.get('email/1/2/fr_FR');
                 return response.data.datas;
             } catch (error) {
