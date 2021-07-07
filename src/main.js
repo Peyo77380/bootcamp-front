5497d3647b6dea4623bdeb776610e56c67b2da81
jean-charles bourseau <jcharles.b@icloud.com>
2021-07-07 15:37:32 +0200

    wallcards - création de cards sur 2 colonnes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import router from './router'
+import Vue from 'vue';
+import './plugins/vuetify';
+import router from './router';
 
-import BootstrapVue from "bootstrap-vue"
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-import App from './App'
+import App from './App';
 
 import Default from './Layout/Wrappers/baseLayout.vue';
 import Pages from './Layout/Wrappers/pagesLayout.vue';
@@ -12,15 +12,16 @@ import Apps from './Layout/Wrappers/appLayout.vue';
 
 Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue);
+Vue.use( BootstrapVue );
+Vue.use( BootstrapVueIcons );
 
-Vue.component('default-layout', Default);
-Vue.component('userpages-layout', Pages);
-Vue.component('apps-layout', Apps);
+Vue.component( 'default-layout', Default );
+Vue.component( 'userpages-layout', Pages );
+Vue.component( 'apps-layout', Apps );
 
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-});
+new Vue( {
+             el        : '#app',
+             router,
+             template  : '<App/>',
+             components: { App }
+         } );
