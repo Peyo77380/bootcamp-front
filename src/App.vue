de1501b14f60b14300b775261d7a8c4b0d19688d
jean-charles bourseau <jcharles.b@icloud.com>
2021-07-07 15:37:32 +0200

    wallcards - création de cards sur 2 colonnes

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
-  <div id="app">
-    <component :is="layout">
-        <transition name="fade" mode="out-in">
-            <router-view></router-view>
-        </transition>
-    </component>
-  </div>
+    <div id="app">
+        <component :is="layout">
+            <transition name="fade" mode="out-in">
+                <router-view></router-view>
+            </transition>
+        </component>
+    </div>
 </template>
 
 <script>
-  const default_layout = "default";
+const default_layout = 'default';
 
-  export default {
+export default {
     computed: {
-      layout() {
-        return (this.$route.meta.layout || default_layout) + '-layout';
-      }
+        layout() {
+            return ( this.$route.meta.layout || default_layout ) + '-layout';
+        }
     }
-  }
+};
 </script>
 
 <style lang="scss">
-  @import "assets/base.scss";
+@import "assets/base.scss";
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+@font-face {
+    font-family : "Montserrat";
+    src         : local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Regular.ttf") format("truetype");
+}
+body{
+    font-family: 'Montserrat', Arial, sans-serif !important;
+}
 </style>
