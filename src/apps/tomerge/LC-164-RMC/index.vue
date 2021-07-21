85ad86c91ed49294e5cbc43df76304e9a95eaf0d
FLOCH Gaëlle <gaelle_floch@hotmail.com>
2021-07-21 14:20:08 +0200

    merge request 164

---
+++ src/apps/tomerge/LC-164-RMC/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <h3>Taux de TVA</h3>
+    </div>
+
+    <Table />
+  </div>
+</template>
+
+<script>
+import Table from "@/apps/tomerge/LC-164-RMC/components/table";
+
+export default {
+  components: {
+    Table,
+  },
+  data: () => ({}),
+
+  methods: {},
+};
+</script>
