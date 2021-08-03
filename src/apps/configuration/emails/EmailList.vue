d56f67a079a66fd5c21e251dcb125f2a7e5362f2
FLOCH Gaëlle <gaelle_floch@hotmail.com>
2021-08-03 17:38:39 +0200

    filter-custom

--- src/apps/configuration/emails/EmailList.vue
+++ src/apps/configuration/emails/EmailList.vue
@@ -1,218 +1,240 @@
 <template>
-    <div>
-        <div class="row justify-content-center">
-            <div
-                class="col-md-2"
-                v-for="category in categories"
-                :key="category.id"
-            >
-                <div class="card mb-3 widget-content" :class="category.class">
-                    <div class="widget-content-wrapper text-white">
-                        <div class="widget-content-left">
-                            <div class="widget-heading">
-                                {{ category.value }}
-                            </div>
-                        </div>
-                        <div class="widget-content-right">
-                            <b-button
-                                class="mb-2 mr-2 btn-icon btn-icon-only btn-pill"
-                                variant="light"
-                                ><i class="pe-7s-glasses btn-icon-wrapper"> </i
-                            ></b-button>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <div
+      class="row"
+      style="display: flex; justify-content: center; align-items: center"
+    >
+      <div
+        class="col-md-3 col-xl-2"
+        v-for="category in categories"
+        :key="category.id"
+      >
+        <div :class="category.class" class="card mb-3 widget-content">
+          <div class="widget-content-wrapper text-white">
+            <div class="widget-content-left">
+              <div class="widget-heading">
+                {{ category.value }}
+              </div>
             </div>
+            <div class="widget-content-right">
+              <v-switch
+                color="primary"
+                v-model="selectedCategory"
+                :value="category.id"
+              ></v-switch>
+            </div>
+          </div>
         </div>
-        <div class="main-card mb-3 card col-12">
-            <div class="card-header-tab card-header">
-                <div
-                    class="card-header-title font-size-lg text-capitalize text-primary font-weight-normal"
-                >
-                    <i
-                        class="header-icon pe-7s-mail-open mr-3 text-muted opacity-6"
-                    ></i>
-                </div>
-                <div class="btn-actions-pane-right actions-icon-btn">
-                    <b-dropdown
-                        toggle-class="btn-icon btn-icon-only"
-                        variant="link"
-                        right
-                    >
-                        <span slot="button-content"
-                            ><i
-                                class=" header-icon lnr-earth mr-3 text-muted opacity-6"
-                            ></i
-                        ></span>
-                        <div>
-                            <button
-                                @click="changeFr"
-                                type="button"
-                                tabindex="0"
-                                class="dropdown-item"
-                            >
-                                <country-flag
-                                    country="FR"
-                                    size="small"
-                                    class="mr-1"
-                                /><span>Français</span>
-                            </button>
-                            <button
-                                @click="changeUs"
-                                type="button"
-                                tabindex="0"
-                                class="dropdown-item"
-                            >
-                                <country-flag
-                                    country="GB"
-                                    size="small"
-                                    class="mr-1"
-                                /><span>Anglais</span>
-                            </button>
-                            <button
-                                type="button"
-                                tabindex="0"
-                                class="dropdown-item"
-                            >
-                                <country-flag
-                                    country="DE"
-                                    size="small"
-                                    class="mr-1"
-                                /><span>Allemand</span>
-                            </button>
-                        </div>
-                    </b-dropdown>
-                </div>
-            </div>
-            <div class="table-responsive">
-                <table
-                    class="align-middle mb-2 table table-borderless table-striped table-hover"
-                >
-                    <thead>
-                        <tr>
-                            <th class="text-center">Catégorie</th>
-                            <th class="text-center">Clé</th>
-                            <th class="text-center">Titre</th>
-                            <th class="text-center">Modifié il y a</th>
-                            <th class="text-center">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="email in emails" :key="email._id">
-                            <td
-                                class="row justify-content-center"
-                                style="font-weight :bold; color:white; opacity: .9; font-size : 11px"
-                            >
-                                <div
-                                    style="display: flex; justify-content: center"
-                                    class="swatch-holder swatch-holder-md"
-                                    :class="getClass(email.section)"
-                                >
-                                    {{ getCategoryInitial(email.section) }}
-                                </div>
-                            </td>
-                            <td class="text-center text-muted">
-                                {{ email.key }}
-                            </td>
-                            <td class="text-center">{{ email.title }}</td>
-                            <td class="text-center">
-                                <time-ago
-                                    :datetime="email.updatedAt"
-                                    locale="fr"
-                                ></time-ago>
-                            </td>
-                            <td class="text-center">
-                                <div>
-                                    <button
-                                        type="button"
-                                        class="btn mb-2 mr-2 btn-icon btn-icon-only btn-pill btn-primary"
-                                        id="editBouton"
-                                        @click="edit(email)"
-                                    >
-                                        <i
-                                            class="lnr-pencil btn-icon-wrapper"
-                                        ></i>
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <b-popover
-                    target="editBouton"
-                    placement="bottomleft"
-                    triggers="hover focus"
-                    content="Modifier"
-                >
-                </b-popover>
-                <b-popover
-                    target="viewBouton"
-                    placement="bottomleft"
-                    triggers="hover focus"
-                    content="Consulter"
-                >
-                </b-popover>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="main-card mb-3 card col-12">
+      <div class="card-header-tab card-header">
+        <div
+          class="
+            card-header-title
+            font-size-lg
+            text-capitalize text-primary
+            font-weight-normal
+          "
+        >
+          <i class="header-icon pe-7s-mail-open mr-3 text-muted opacity-6"></i>
+        </div>
+        <div class="btn-actions-pane-right actions-icon-btn">
+          <b-dropdown
+            toggle-class="btn-icon btn-icon-only"
+            variant="link"
+            right
+          >
+            <span slot="button-content"
+              ><i class="header-icon lnr-earth mr-3 text-muted opacity-6"></i
+            ></span>
+            <div>
+              <button
+                @click="changeFr"
+                type="button"
+                tabindex="0"
+                class="dropdown-item"
+              >
+                <country-flag country="FR" size="small" class="mr-1" /><span
+                  >Français</span
+                >
+              </button>
+              <button
+                @click="changeUs"
+                type="button"
+                tabindex="0"
+                class="dropdown-item"
+              >
+                <country-flag country="GB" size="small" class="mr-1" /><span
+                  >Anglais</span
+                >
+              </button>
+              <button type="button" tabindex="0" class="dropdown-item">
+                <country-flag country="DE" size="small" class="mr-1" /><span
+                  >Allemand</span
+                >
+              </button>
+            </div>
+          </b-dropdown>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <table
+          class="
+            align-middle
+            mb-2
+            table table-borderless table-striped table-hover
+          "
+        >
+          <thead>
+            <tr>
+              <th class="text-center">Catégorie</th>
+              <th class="text-center">Clé</th>
+              <th class="text-center">Titre</th>
+              <th class="text-center">Modifié il y a</th>
+              <th class="text-center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="email in filteredItems" :key="email._id">
+              <td
+                class="row justify-content-center"
+                style="
+                  font-weight: bold;
+                  color: white;
+                  opacity: 0.9;
+                  font-size: 11px;
+                "
+              >
+                <div
+                  style="display: flex; justify-content: center"
+                  class="swatch-holder swatch-holder-md"
+                  :class="getClass(email.section)"
+                >
+                  {{ getCategoryInitial(email.section) }}
+                </div>
+              </td>
+              <td class="text-center text-muted">
+                {{ email.key }}
+              </td>
+              <td class="text-center">{{ email.title }}</td>
+              <td class="text-center">
+                <time-ago :datetime="email.updatedAt" locale="fr"></time-ago>
+              </td>
+              <td class="text-center">
+                <div>
+                  <button
+                    type="button"
+                    class="
+                      btn
+                      mb-2
+                      mr-2
+                      btn-icon btn-icon-only btn-pill btn-primary
+                    "
+                    id="editBouton"
+                    @click="edit(email)"
+                  >
+                    <i class="lnr-pencil btn-icon-wrapper"></i>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <b-popover
+          target="editBouton"
+          placement="bottomleft"
+          triggers="hover focus"
+          content="Modifier"
+        >
+        </b-popover>
+        <b-popover
+          target="viewBouton"
+          placement="bottomleft"
+          triggers="hover focus"
+          content="Consulter"
+        >
+        </b-popover>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import CountryFlag from "vue-country-flag";
 import moment from "moment";
 export default {
-    name: "email-list",
-    components: {
-        CountryFlag
-    },
+  name: "email-list",
+  components: {
+    CountryFlag,
+  },
 
-    data() {
-        return {
-            isActive: true,
-            displayDetails: false
-        };
-    },
+  data() {
+    return {
+      isActive: true,
+      displayDetails: false,
+      selectedCategory: "Toutes les catégories",
+    };
+  },
 
-    props: {
-        emails: {
-            type: Array
-        },
-        categories: {
-            type: Array
-        }
+  props: {
+    emails: {
+      type: Array,
     },
+    categories: {
+      type: Array,
+    },
+  },
 
-    methods: {
-        edit(email) {
-            this.$emit("edit", email);
-        },
-        changeUs() {
-            this.$emit("changeUs");
-        },
-        changeFr() {
-            this.$emit("changeFr");
-        },
-        getCategoryInitial(id) {
-            const category = this.categories.filter(item => {
-                return item.id == id;
-            });
-            if (category[0]) {
-                return category[0].initial;
-            }
-            return "";
-        },
-        getClass(id) {
-            const category = this.categories.filter(item => {
-                return item.id == id;
-            });
-            if (category[0]) {
-                return category[0].class;
-            }
-            return "";
-        },
-        getElapsedTime(time) {
-            moment.locale("fr");
-            return moment(time).fromNow();
-        }
-    }
+  computed: {
+    filteredItems: function () {
+      var vm = this;
+      var category = vm.selectedCategory;
+
+      if (category === "Toutes les catégories" || category === null) {
+        return vm.emails;
+      } else {
+        return vm.emails.filter(function (email) {
+          return email.section === category;
+        });
+      }
+    },
+    prows() {
+      return this.filteredItems.length;
+    },
+  },
+
+  methods: {
+    edit(email) {
+      this.$emit("edit", email);
+    },
+    changeUs() {
+      this.$emit("changeUs");
+    },
+    changeFr() {
+      this.$emit("changeFr");
+    },
+    getCategoryInitial(id) {
+      const category = this.categories.filter((item) => {
+        return item.id == id;
+      });
+      if (category[0]) {
+        return category[0].initial;
+      }
+      return "";
+    },
+    getClass(id) {
+      const category = this.categories.filter((item) => {
+        return item.id == id;
+      });
+      if (category[0]) {
+        return category[0].class;
+      }
+      return "";
+    },
+    getElapsedTime(time) {
+      moment.locale("fr");
+      return moment(time).fromNow();
+    },
+  },
 };
 </script>
--- src/apps/configuration/emails/index.vue
+++ src/apps/configuration/emails/index.vue
@@ -1,38 +1,34 @@
 <template>
+  <div>
     <div>
-        <div>
-            <page-title
-                :heading="heading"
-                :subheading="subheading"
-                :icon="icon"
-            />
-        </div>
-        <div>
-            <email-list
-                :emails="emails"
-                :categories="categories"
-                @displayEmail="showDetails"
-                @edit="editEmail"
-                @changeUs="translateUs"
-                @changeFr="translateFr"
-            />
-        </div>
-        <div>
-            <display-email
-                :details="emailDetails"
-                @closeDisplay="displayOff"
-                :dialog="behavior.modalDisplay"
-            />
-        </div>
-        <div>
-            <edit-email
-                @saveModification="modificationEmail"
-                :editedEmail="editedEmail"
-                :dialog="behavior.modalEdit"
-                @close="closeEdit"
-            />
-        </div>
+      <page-title :heading="heading" :subheading="subheading" :icon="icon" />
     </div>
+    <div>
+      <email-list
+        :emails="emails"
+        :categories="categories"
+        @displayEmail="showDetails"
+        @edit="editEmail"
+        @changeUs="translateUs"
+        @changeFr="translateFr"
+      />
+    </div>
+    <div>
+      <display-email
+        :details="emailDetails"
+        @closeDisplay="displayOff"
+        :dialog="behavior.modalDisplay"
+      />
+    </div>
+    <div>
+      <edit-email
+        @saveModification="modificationEmail"
+        :editedEmail="editedEmail"
+        :dialog="behavior.modalEdit"
+        @close="closeEdit"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -43,75 +39,74 @@ import { Emails } from "@/mixins/email";
 import { Globals } from "@/mixins/global";
 
 export default {
-    components: {
-        PageTitle,
-        EmailList,
-        EditEmail
+  components: {
+    PageTitle,
+    EmailList,
+    EditEmail,
+  },
+  mixins: [Emails, Globals],
+  async mounted() {
+    await this.loadCategories();
+    await this.loadEmails();
+  },
+  data() {
+    return {
+      heading: "Email de services",
+      subheading: "Créer l'ensemble des modèles de vos email de services ici.",
+      icon: "pe-7s-mail",
+      emails: [],
+      categories: [],
+      dialog: false,
+      emailDetails: {},
+      editedEmail: {},
+      behavior: {
+        modalEdit: false,
+        modalDisplay: false,
+      },
+      editedIndex: "",
+    };
+  },
+  methods: {
+    showDetails(email) {
+      this.behavior.modalDisplay = true;
+      this.emailDetails = email;
     },
-    mixins: [Emails, Globals],
-    async mounted() {
-        await this.loadCategories();
-        await this.loadEmails();
+    displayOff() {
+      this.behavior.modalDisplay = false;
     },
-    data() {
-        return {
-            heading: "Email de services",
-            subheading:
-                "Créer l'ensemble des modèles de vos email de services ici.",
-            icon: "pe-7s-mail",
-            emails: [],
-            categories: [],
-            dialog: false,
-            emailDetails: {},
-            editedEmail: {},
-            behavior: {
-                modalEdit: false,
-                modalDisplay: false
-            },
-            editedIndex: ""
-        };
+    editEmail(email) {
+      this.behavior.modalEdit = true;
+      this.editedEmail = email;
     },
-    methods: {
-        showDetails(email) {
-            this.behavior.modalDisplay = true;
-            this.emailDetails = email;
-        },
-        displayOff() {
-            this.behavior.modalDisplay = false;
-        },
-        editEmail(email) {
-            this.behavior.modalEdit = true;
-            this.editedEmail = email;
-        },
-        closeEdit() {
-            this.behavior.modalEdit = false;
-        },
-        modificationEmail() {
-            this.editedIndex = this.emails.indexOf(this.editedEmail);
-            this.$sweetNotif("Modification de l'email réussie !");
-        },
-        translateUs() {
-            this.emails = this.EmailDataUs;
-            //TODO point API pour importer les data en anglais
-        },
-        translateFr() {
-            this.emails = this.EmailDataFr;
-            //TODO point API pour importer les data en francais
-        },
-        async loadEmails() {
-            try {
-                this.emails = await this.getAllEmails();
-            } catch (error) {
-                this.$sweetError("GLC-30");
-            }
-        },
-        async loadCategories() {
-            try {
-                this.categories = await this.getGlobals();
-            } catch (error) {
-                this.$sweetError("GLC-30");
-            }
-        }
-    }
+    closeEdit() {
+      this.behavior.modalEdit = false;
+    },
+    modificationEmail() {
+      this.editedIndex = this.emails.indexOf(this.editedEmail);
+      this.$sweetNotif("Modification de l'email réussie !");
+    },
+    translateUs() {
+      this.emails = this.EmailDataUs;
+      //TODO point API pour importer les data en anglais
+    },
+    translateFr() {
+      this.emails = this.EmailDataFr;
+      //TODO point API pour importer les data en francais
+    },
+    async loadEmails() {
+      try {
+        this.emails = await this.getAllEmails();
+      } catch (error) {
+        this.$sweetError("GLC-30");
+      }
+    },
+    async loadCategories() {
+      try {
+        this.categories = await this.getGlobals();
+      } catch (error) {
+        this.$sweetError("GLC-30");
+      }
+    },
+  },
 };
 </script>
