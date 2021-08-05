d2ab2e730042acbe2f393c4f1a63c9b0fa208585
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-08-05 14:50:39 +0200

    Change header

--- src/apps/tomerge/LC-168/components/PageTitle2.vue
+++ src/apps/tomerge/LC-168/components/PageTitle2.vue
@@ -3,43 +3,40 @@
         <div class="page-title-wrapper">
             <div class="page-title-heading">
                 <div class="page-title-icon">
-                    <i :class="icon"/>
+                    <i :class="icon" />
                 </div>
                 <div>
-                    {{heading}}
-                    <div
-                        class="page-title-subheading">
-                        {{subheading}}
+                    {{ heading }}
+                    <div class="page-title-subheading">
+                        {{ subheading }}
                     </div>
                 </div>
             </div>
-            
+            <div class="page-title-actions">
+                <button
+                    type="button"
+                    @click="openModal()"
+                    class="btn-shadow d-inline-flex align-items-center btn btn-success"
+                >
+                    Ajouter un nouvel attribut
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {library} from '@fortawesome/fontawesome-svg-core'
-    import {
-        faStar,
-        faPlus
-    } from '@fortawesome/free-solid-svg-icons'
-    import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-
-    library.add(
-        faStar,
-        faPlus,
-    );
-
-    export default {
-        components: {
-            'font-awesome-icon': FontAwesomeIcon,
-        },
-        props: {
-            icon: String,
-            heading: String,
-            subheading: String,
-
+export default {
+   
+    props: {
+        icon: String,
+        heading: String,
+        subheading: String
+    },
+    methods: {
+        openModal() {
+            this.$emit("openModal");
         }
     }
+};
 </script>
